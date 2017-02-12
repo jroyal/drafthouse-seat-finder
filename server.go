719a70cce0fe6bff69e2a10a82f8b9aea79cc479
James Royal <jhr.atx@gmail.com>
2017-02-11 18:05:11 -0600

    Original html file

--- server.go
+++ server.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/jroyal/drafthouse-seat-finder/drafthouse"
 	"github.com/labstack/echo"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
+
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = t
+
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Drafthouse Seat Finder")
+		return c.Render(http.StatusOK, "index", "World")
 	})
+
 	e.GET("/movies", drafthouse.HandleGetMovies)
 	e.GET("/movies/:film-slug", drafthouse.HandleGetSingleMovie)
 	e.Logger.Fatal(e.Start("localhost:8080"))
