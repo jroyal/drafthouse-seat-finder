bc2ef7f1240f0a1813de75352107942346928fd7
James Royal <jhr.atx@gmail.com>
2017-02-12 17:49:55 -0600

    Only hold the feed for 30 minutes before refreshing

--- drafthouse/collector.go
+++ drafthouse/collector.go
@@ -12,6 +12,8 @@ var myClient = &http.Client{Timeout: 10 * time.Second}
 
 var market = MarketResponse{}
 
+var lastUpdated time.Time
+
 func getJson(url string, target interface{}) error {
 	r, err := myClient.Get(url)
 	if err != nil {
@@ -22,10 +24,16 @@ func getJson(url string, target interface{}) error {
 	return json.NewDecoder(r.Body).Decode(target)
 }
 
+func isStale() bool {
+	age := time.Since(lastUpdated)
+	return age.Minutes() > 30
+}
+
 func getMarketInfo() Market {
-	if market.Market.MarketID == "" {
+	if market.Market.MarketID == "" || isStale() {
 		log.WithField("market", "austin").Info("Getting Feed Information")
 		getJson(austinMarketFeed, &market)
+		lastUpdated = time.Now()
 	}
 
 	return market.Market
