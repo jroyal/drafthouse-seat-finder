9821a8ff76684551cd64fc5351b9aa2d5ba7adcc
James Royal <jhr.atx@gmail.com>
2017-02-18 15:19:01 -0600

    Add dockerfile and update script

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM iron/base
+WORKDIR /app
+EXPOSE 8080
+COPY . /app/
+CMD ["./drafthouse-seat-finder"]
---
+++ update_deployment.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf drafthouse-seat-finder
+CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" .
+docker build -t jroyal/drafthouse-seat-finder .
+docker stop drafthouse-seat-finder
+docker container prune -f
+docker image prune -f
+docker run -d --name drafthouse-seat-finder -p 8080:8080 jroyal/drafthouse-seat-finder
